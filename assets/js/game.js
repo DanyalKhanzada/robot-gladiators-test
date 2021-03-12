58ff8f05ecc5209759346ea019349701f42602e2
“Danyal <danyalkhanzada89@gmail.com>
2021-03-12 17:21:12 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,14 +28,17 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -54,7 +57,7 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      playerHealth = Math.max(0, playerHealth - enemyAttack);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -72,7 +75,7 @@ var fight = function(enemyName) {
 
   // function to start a new game
   var startGame = function() {
-    playerHealth = 100;
+    enemyHealth = randomNumber(40, 60);
     playerAttack = 10;
     playerMoney = 10;
   for (var i = 0; i < enemyNames.length; i++) {
@@ -80,7 +83,7 @@ var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
     
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = Math.floor(Math.random() * 21) + 40;
     fight(pickedEnemyName);
     // if we're not at the last enemy in the array
     if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -152,6 +155,13 @@ switch (shopOptionPrompt) {
     shop();
     break;
 }
+};
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 // start the game when the page loads
